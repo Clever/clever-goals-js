11cea6a7fda2659e3fa65fe6e465a8a3643e428e
Jonah Kagan <jonah.kagan@clever.com>
2017-06-26 12:28:47 -0700

    sketch usage tracking

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,5 @@
-import * as fetch from "whatwg-fetch";
+import "whatwg-fetch";
+import * as timeme from "timeme.js";
 
 export default class CleverGoals {
   districtToken;
@@ -36,5 +37,46 @@ export default class CleverGoals {
 
   startTrackingUsage() {
     console.log("Clever Goals: Starting usage tracking.");
+
+    const lock = (() => {
+      let locked = false;
+      let timeout;
+      return {
+        acquire: () => {
+          if (locked) { return false; }
+          locked = true;
+          timeout = setTimeout(() => locked = false, 60000); // timeout to release lock
+          return true;
+        },
+        release: () => {
+          if (timeout) {
+            clearTimeout(timeout);
+            timeout = null;
+          }
+          locked = false;
+        },
+      };
+    })();
+
+    timeme.initialize({
+      idleTimeoutInSeconds: 60,
+    });
+
+    let totalSecondsSent = 0;
+
+    setInterval(async () => {
+      if (!lock.acquire()) { return; }
+      const currTotalSeconds = timeme.getTimeOnCurrentPageInSeconds();
+      const delta =  currTotalSeconds - totalSecondsSent;
+      if (delta === 0) { return; }
+      try {
+        await this.recordIncrementalUsage(delta / 60);
+        totalSecondsSent = currTotalSeconds;
+      } catch (err) {
+        console.error("Clever Goals:", err)
+      } finally {
+        lock.release();
+      }
+    }, 5000);
   }
 }
